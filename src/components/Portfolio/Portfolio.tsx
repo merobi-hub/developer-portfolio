c1e3b7e93f065cfb58dff7ba0f3964b31831a37b
merobi-hub <merobi@gmail.com>
2023-12-26 17:03:18 -0500

    Remove unused import and deprecated import from portfolio.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -11,8 +11,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import karma_typewriter from '../../assets/images/karma_typewriter.jpg';
 import { Navbar } from '../../components';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
-import bibliomania_screenshot from '../../assets/images/bibliomania_output_screenshot.png'; 
+import { createTheme, ThemeProvider } from '@material-ui/core';
 import weather_screenshot from '../../assets/images/weather_app_screenshot.png';
 import car_screenshot from '../../assets/images/car_api.png';
 import racer_screenshot from '../../assets/images/racer.png';
@@ -72,7 +71,7 @@ const useStyles = makeStyles({
     }
   });
 
-const theme = createMuiTheme({
+const theme = createTheme({
     typography: { 
         fontFamily: 'Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace'
     },
